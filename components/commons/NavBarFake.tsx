79f32f2664c00509f35868d673273ddef494ca30
teknixproject <203745776+teknixproject@users.noreply.github.com>
2025-05-23 17:02:40 +0700

    Update NavBarFake.tsx

--- components/commons/NavBarFake.tsx
+++ components/commons/NavBarFake.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
-import Link from 'next/link';
+// import Link from 'next/link';
 import { FiHome } from 'react-icons/fi';
 
 export default function NavBar() {
